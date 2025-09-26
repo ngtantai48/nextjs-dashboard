de423668d96d705f5fc3bcc09ad7bcf033e31985
NguyenTanTai <ngtantai48@gmail.com>
2025-09-26 07:36:04 +0700

    Add layout and dashboard structure with navigation; integrate global styles and fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const interFont = Inter({ subsets: ['latin'] });
+export const lusitanaFont = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
