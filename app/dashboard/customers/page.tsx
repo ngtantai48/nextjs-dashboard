8c54e1acba029355b898d712ddd27ec85ba556e6
NguyenTanTai <ngtantai48@gmail.com>
2025-09-28 01:39:12 +0700

    Implement authentication with NextAuth, including user login via credentials and middleware for protected routes

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Customers',
+    description: 'Manage your customers',
+};
+
 export default function Page() {
     return <p>Customers Page</p>;
 }
